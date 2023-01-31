029b92c17ecd95b724da13d1a69767feff98a39d
LeivaDiego <lei21752@uvg.edu.gt>
2023-01-30 20:02:59 -0600

    Ver 2.0 - Lector de archivos
    Método para la lectura  y almacenado de la información de archivos completada

--- Postfix_Calculator/DriverProgram.java
+++ Postfix_Calculator/DriverProgram.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DriverProgram {
     public static void main(String[] args) {
+        System.out.println("Bienvenido a la calculadora Postfix\n");
+        System.out.println("Porfavor ingrese la ruta del archivo a calcular: \n");
+        Scanner input = new Scanner(System.in);
+        String filePath = input.nextLine();
+        ArrayList<String> fileData = FileReader(filePath);
+        System.out.println(fileData);
 
+    }
+
+    public static ArrayList FileReader(String path) {
+        ArrayList<String> data = null;
+        try {
+            File rawData = new File(path);
+            Scanner reader = new Scanner(rawData);
+            String line;
+            data = new ArrayList<String>();
+            while (reader.hasNext()) {
+                line = reader.nextLine();
+                data.add(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
+        return data;
     }
 }
