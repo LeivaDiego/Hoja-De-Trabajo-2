5e5c1a9bebc5e64b92ed3bdd16bd5b0b7ce74e3c
LeivaDiego <lei21752@uvg.edu.gt>
2023-01-30 20:45:28 -0600

    Ver. 3.1 - Correcciones de clase Stack
    Correccion de clase stack

--- Postfix_Calculator/Stack.java
+++ Postfix_Calculator/Stack.java
@@ -1,26 +1,39 @@
-public class Stack implements IStack{
+import java.util.ArrayList;
+
+public class Stack<T> implements IStack{
+
+    private ArrayList<T> values;
+
+    public Stack(){
+        values = new ArrayList<T>();
+    }
+
     @Override
     public int count() {
-        return 0;
+        return values.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return values.isEmpty();
     }
 
     @Override
     public void push(Object value) {
-
+        values.add(0, (T) value);
     }
 
     @Override
     public Object pull() {
-        return null;
+        if (!isEmpty()){
+            return values.remove(0);
+        }else return null;
     }
 
     @Override
     public Object peek() {
-        return null;
+        if (!isEmpty()){
+            return values.get(0);
+        }else return null;
     }
 }
